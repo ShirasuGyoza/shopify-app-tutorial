15a5284529feea5d68a9249b0d9714e3a6e2677a
Akane Nakayama <nakayama.a@tam-tam.co.jp>
2020-05-19 18:16:59 +0900

    complete Polaris tutorial

--- sample-embedded-app/server.js
+++ sample-embedded-app/server.js
@@ -26,11 +26,16 @@ app.prepare().then(() => {
       scopes: ['read_products'],
       afterAuth(ctx) {
         const { shop, accessToken } = ctx.session;
-
+        ctx.cookies.set('shopOrigin', shop, {
+          httpOnly: false,
+          secure: true,
+          sameSite: 'none'
+        });
         ctx.redirect('/');
       },
     }),
   );
+
 
   server.use(verifyRequest());
   server.use(async (ctx) => {
